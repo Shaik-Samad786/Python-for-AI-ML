e0b84fd0d911f197fd7cb036c3254109e1704248
Shaik Samad <samadshaik2008786@gmail.com>
2026-02-13 09:59:14 +0530

    Create Triangle_angle_and_type.py

---
+++ basic_problem_solving/Triangle_angle_and_type.py
@@ -0,0 +1,11 @@
+a = int(input("Enter one angle of Triangle: "))
+b = int(input("Enter another angle of Triangle: "))
+c = 180 - (a+b)
+print("The Thrid angle of Triangle = ", c)
+
+if (a==90 or b==90 or c==90) :
+    print("The triangle formed is Right-Angled Triangle")
+elif (a==b or b==c or c==a):
+    print("The triangle formed is Isosceles Triangle")
+else:
+    print("Neither")
