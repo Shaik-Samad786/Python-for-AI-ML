89b3ed783d061cca3c42eea4d98162a947867904
Shaik Samad <samadshaik2008786@gmail.com>
2026-02-14 17:34:37 +0530

    Create bouncing_ball_distance.py

---
+++ basic_problem_solving/bouncing_ball_distance.py
@@ -0,0 +1,5 @@
+Y = float(input("Enter the initial height (meters): "))
+
+distance = Y + 2 * (Y / 2) + 2 * (Y / 4)
+
+print("Total distance, ball touches ground for the third time:", distance, "meters")
