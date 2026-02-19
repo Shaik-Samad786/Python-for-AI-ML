2f5aea0338d84cad55e4d5d0ace0618628084ca7
Shaik Samad <samadshaik2008786@gmail.com>
2026-02-19 09:47:22 +0530

    Create List_dimens.py

---
+++ List_dimens.py
@@ -0,0 +1,23 @@
+a = [[1,2],[2,3]]
+print(type(a))
+print(a)
+b =[1,2,3,4,5,6,7,8,9]
+print(type(b))
+print(b)
+print(a[0])
+print(a[0][0])
+print(a[0][1])
+print(type(a[0][0]))
+
+
+
+# output
+
+<class 'list'>
+[[1, 2], [2, 3]]
+<class 'list'>
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
+[1, 2]
+1
+2
+<class 'int'>
