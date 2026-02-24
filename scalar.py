ec72771b79e892622f004cc3ee9f8eacda411c19
Shaik Samad <samadshaik2008786@gmail.com>
2026-02-24 16:14:25 +0530

    Update scalar.py

--- scalar.py
+++ scalar.py
@@ -15,3 +15,12 @@ for i in range(len(A)):
     for j in range(len(A[0])):
         D[i][j] =  A[i][j] - B[i][j]
 print(D)
+
+# scalar_multi
+A = [[1,2],[3,4]]
+B = [[5,6],[7,8]]
+E = A
+for i in range(len(A)):
+    for j in range(len(A[0])):
+        E[i][j] =  A[i][j] * B[i][j]
+print(E)
