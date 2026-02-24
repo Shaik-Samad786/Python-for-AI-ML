73d3ef880fe0679c33e8559c1aa53e09cea07e9e
Shaik Samad <samadshaik2008786@gmail.com>
2026-02-24 16:02:05 +0530

    Update scalar.py

--- scalar.py
+++ scalar.py
@@ -1,3 +1,4 @@
+# scalar_addition
 A = [[1,2],[3,4]]
 B = [[5,6],[7,8]]
 C = A
@@ -5,3 +6,12 @@ for i in range(len(A)):
     for j in range(len(A[0])):
         C[i][j] =  A[i][j] + B[i][j]
 print(C)
+
+# scalar_substraction
+A = [[1,2],[3,4]]
+B = [[5,6],[7,8]]
+D = A
+for i in range(len(A)):
+    for j in range(len(A[0])):
+        D[i][j] =  A[i][j] - B[i][j]
+print(D)
